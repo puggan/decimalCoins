57b7ff7c9db65b12dc039352d18a946c2e46973c
Thorbjörn Sundragon <puggan@spiro.se>
2020-07-27 19:42:34 +0200

    simplify

--- src/main/java/se/puggan/decimal_coins/DecimalCoins.java
+++ src/main/java/se/puggan/decimal_coins/DecimalCoins.java
@@ -1,40 +1,28 @@
 package se.puggan.decimal_coins;
 
+import net.minecraft.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLDedicatedServerSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import se.puggan.decimal_coins.utils.RegistryHandler;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("decimal_coins")
 public class DecimalCoins {
     // Directly reference a log4j logger.
-    public static final Logger LOGGER = LogManager.getLogger();
+    //public static final Logger LOGGER = LogManager.getLogger();
     public static String MOD_ID = "decimal_coins";
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, se.puggan.decimal_coins.DecimalCoins.MOD_ID);
 
     public DecimalCoins() {
-        // Register the setup method for modloading
+        int[] values = {1, 5, 10, 20, 100};
+
+        for(int value : values) {
+            ITEMS.register("coin_" + value, Coin::new);
+        }
+
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-        modEventBus.addListener(this::setup);
-        modEventBus.addListener(this::setupClient);
-        modEventBus.addListener(this::setupServer);
-
-        RegistryHandler.init();
-    }
-
-    private void setup(final FMLCommonSetupEvent event) {
-    }
-
-    private void setupClient(final FMLClientSetupEvent event) {
-        RegistryHandler.initClientOnly();
-    }
-
-    private void setupServer(final FMLDedicatedServerSetupEvent event) {
-        RegistryHandler.initServerOnly();
+        ITEMS.register(modEventBus);
     }
 }
